f3bafbb02d09e108d9392099f1b63f1025e0719d
Filipez <filipezmarron@hotmail.com>
2021-10-17 18:26:45 -0300

    test: ensure CompareFieldsValidation returns InvalidParamError if validation fails and not return if it succeds

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,33 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+interface SutTypes {
+  sut: CompareFieldsValidation
+}
+const makeSut = (): SutTypes => {
+  const sut = new CompareFieldsValidation('any_field', 'any_field_to_compare')
+  return { sut }
+}
+describe('CompareFieldsValidation', () => {
+  it('should return a InvalidParamError if validation fails', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({
+      any_field: 'any_value',
+      any_field_to_compare: 'wrong_value'
+    })
+
+
+    expect(error).toEqual(new InvalidParamError('any_field_to_compare'));
+  });
+  it('should not return if validation succeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({
+      any_field: 'any_value',
+      any_field_to_compare: 'any_value'
+    })
+
+
+    expect(error).toBeFalsy();
+  });
+
+})
