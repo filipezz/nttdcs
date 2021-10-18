f103f5d4b87dedf2e83cb2a4fc08612d8455c84b
Filipez <filipezmarron@hotmail.com>
2021-10-18 18:59:29 -0300

    refactor: move data layer interfaces to folders

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,4 @@
-import { AddAccountRepository } from "../../../../data/protocols/add-account-repository";
+import { AddAccountRepository } from "../../../../data/protocols/db/add-account-repository";
 import { MongoHelper } from "../helpers/mongo-helper";
 import { AccountMongoRepository } from "./account";
 
