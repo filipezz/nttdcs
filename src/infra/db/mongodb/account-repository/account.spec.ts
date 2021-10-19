a428a2175b872828960fbfcc67f50e03f878024a
Filipez <filipezmarron@hotmail.com>
2021-10-19 18:01:40 -0300

    feat: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -54,4 +54,21 @@ describe("Account Mongo Repository", () => {
     expect(account).toBeFalsy();
 
   });
+  test("Should uptade the account accessToken on updateAccessToken success", async () => {
+    const sut = makeSut();
+    const res = await accountCollection.insertOne({
+      name: "any_name",
+      email: "any_email@email.com",
+      password: "any_password",
+    });
+    const fakeAccount = await accountCollection.findOne({ _id: res.insertedId })
+    expect(fakeAccount.accessToken).toBeFalsy()
+    await sut.updateAccessToken(fakeAccount._id, "any_token");
+    const account = await accountCollection.findOne({ _id: fakeAccount._id })
+    expect(account).toBeTruthy();
+    expect(account.accessToken).toBe("any_token");
+
+
+
+  });
 });
