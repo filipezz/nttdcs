d75a37ac4c783d4fbc0c67859a63d5c6379d3acd
Filipez <filipezmarron@hotmail.com>
2021-10-23 16:23:29 -0300

    test: ensure login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -44,5 +44,15 @@ describe("Login Routes", () => {
         })
         .expect(200);
     });
+    test("Should return an 401 on login", async () => {
+
+      await request(app)
+        .post("/api/login")
+        .send({
+          email: "json@email.com",
+          password: "123456",
+        })
+        .expect(401);
+    });
   })
 });
